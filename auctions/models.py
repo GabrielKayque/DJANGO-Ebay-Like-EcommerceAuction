81b4cfc7af495ed07562248a4e84bf636554da40
GabrielKayque <mr.biiiel@hotmail.com>
2022-04-19 03:07:38 -0300

    created Models for Listing and Category

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,23 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=25)
+    
+    
+class Listing(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    description = models.TextField()
+    bid = models.FloatField()
+    imgurl = models.URLField(blank=True)
+    categories = models.ManyToManyField(Category, on_delete=models.PROTECT, related_name="listings")
+    created_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.title} | {self.author}"
+
