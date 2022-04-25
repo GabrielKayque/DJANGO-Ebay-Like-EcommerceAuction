c79528e5237317229235d385fcf703fbbf35f92d
GabrielKayque <mr.biiiel@hotmail.com>
2022-04-25 02:13:36 -0300

    More responsive design

--- auctions/urls.py
+++ auctions/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("auction/<int:pk>", views.auction_page, name="auction-page")
+    path("auction/<int:pk>", views.auction_page, name="auction-page"),
+    # path("auction/<str:username>/new_auction", views.create_page, name="createpage"),
 ]
