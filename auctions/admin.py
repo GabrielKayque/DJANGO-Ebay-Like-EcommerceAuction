b42eb422b68cc1ac038a9a93498f9fe80a0ff38d
GabrielKayque <mr.biiiel@hotmail.com>
2022-04-27 23:42:58 -0300

    Admin model changed

--- auctions/admin.py
+++ auctions/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Category, Listing, User, Watchlist
 
 # Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    filter_horizontal = ("categories",)
 
 admin.site.register(User)
 admin.site.register(Category)
-admin.site.register(Listing)
+admin.site.register(Listing, ListingAdmin)
 admin.site.register(Watchlist)
